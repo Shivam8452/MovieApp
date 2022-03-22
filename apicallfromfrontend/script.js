a3eba0295b90d55ebfeb50277950f78b0e04bb98
Shivam <19148@iiitu.ac.in>
2022-03-22 11:20:33 +0530

    update

--- apicallfromfrontend/script.js
+++ apicallfromfrontend/script.js
@@ -163,3 +163,30 @@ let searchInput = document.getElementById("search");
         .filter(resp => resp.status !== false)
         .distinctUntilChanged((a, b) => a.result_hash === b.result_hash)
         .subscribe(resp => showMovies(resp.details));
+
+        // Theme toggle
+         // function to set a given theme/color-scheme
+         function setTheme(themeName) {
+          localStorage.setItem('theme', themeName);
+          document.documentElement.className = themeName;
+      }
+
+      // function to toggle between light and dark theme
+      function toggleTheme() {
+          if (localStorage.getItem('theme') === 'theme-dark') {
+              setTheme('theme-light');
+          } else {
+              setTheme('theme-dark');
+          }
+      }
+
+      // Immediately invoked function to set the theme on initial load
+      (function () {
+          if (localStorage.getItem('theme') === 'theme-dark') {
+              setTheme('theme-dark');
+              document.getElementById('slider').checked = false;
+          } else {
+              setTheme('theme-light');
+            document.getElementById('slider').checked = true;
+          }
+      })();
